deadb628fb499011dadafb799990faf0ed7df368
Jennifer Lee <jenlee.tech@gmail.com>
2018-11-02 00:07:15 -0400

    added properties to handle errors like no shelf, no author

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -10,8 +10,9 @@ class Books extends Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage: `url(${this.props.book.imageLinks &&
-                this.props.book.imageLinks.thumbnail})`
+              backgroundImage: `url(${(this.props.book.imageLinks &&
+                this.props.book.imageLinks.thumbnail) ||
+                "Blank Cover"})`
             }}
           />
           <div className="book-shelf-changer">
@@ -19,20 +20,22 @@ class Books extends Component {
               onChange={event =>
                 this.props.changeShelf(this.props.book, event.target.value)
               }
-              value={this.props.currentShelf}
+              value={this.props.currentShelf || "None"}
             >
               <option value="move" disabled>
                 Move to...
-              </option>{" "}
-              <option value="currentlyReading"> Currently Reading </option>{" "}
-              <option value="wantToRead"> Want to Read </option>{" "}
-              <option value="read"> Read </option>{" "}
-              <option value="none"> None </option>{" "}
-            </select>{" "}
-          </div>{" "}
-        </div>{" "}
-        <div className="book-title"> {this.props.book.title} </div>{" "}
-        <div className="book-authors"> {this.props.book.authors} </div>{" "}
+              </option>
+              <option value="currentlyReading"> Currently Reading </option>
+              <option value="wantToRead"> Want to Read </option>
+              <option value="read"> Read </option>
+              <option value="none"> None </option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title"> {this.props.book.title} </div>
+        <div className="book-authors">
+          {this.props.book.authors || "No Author found"}
+        </div>
       </div>
     );
   }
