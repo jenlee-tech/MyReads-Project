4aa916dfcf17c09ca199e5f80fce9d41fa157962
Jennifer Lee <jenlee.tech@gmail.com>
2018-11-03 02:15:10 -0400

    deleted files - went from jsx to js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-  BrowserRouter
-} from "react-router-dom";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render( <
-  BrowserRouter >
-  <
-  App / >
-  <
-  /BrowserRouter>,
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
